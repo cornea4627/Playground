02d825fc85a89d1edda2e74e343066e8be5fcda0
cornea4627 <137205831+cornea4627@users.noreply.github.com>
2023-07-03 13:25:49 -0700

    update githubdemo/index.js

--- githubdemo/index.js
+++ githubdemo/index.js
@@ -4,7 +4,7 @@ const githubToken = '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';
 
 const resultElement = document.getElementById('result');
 
-axios.get('https://api.github.com/users/cornea4627', {
+fetch.get('https://api.github.com/users/cornea4627', {
   headers: {
     Authorization: `token ${githubToken}`
   }
