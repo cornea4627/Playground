faff75b3270f4d25ab1779164c13da58681cce1e
cornea4627 <cornea.wc59h@slmail.me>
2023-07-23 10:47:56 -0700

    update

---
+++ Python/07 Modules/modules.py
@@ -0,0 +1,5 @@
+import greetings
+
+name = input("What is your name? ")
+
+greetings.say_hello(name)
---
+++ Python/While/while.py
@@ -0,0 +1,7 @@
+name = ""
+while name != "Juan":
+    name = input("Please enter your correct name: ")
+    if name != "Juan":
+        print(name, "is not the right answer")
+
+print("thats correct!")
